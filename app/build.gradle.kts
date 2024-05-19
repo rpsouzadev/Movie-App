0cbfb0a331c858deedf5b5abdbb3dcd691e9e6f0
Rafael Souza <rpsouza.dev@gmail.com>
2024-05-19 16:04:04 -0300

    Add firebase

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
   alias(libs.plugins.android.application)
   alias(libs.plugins.jetbrains.kotlin.android)
+  id("com.google.gms.google-services")
 }
 
 android {
@@ -39,6 +40,13 @@ dependencies {
   implementation(libs.material)
   implementation(libs.androidx.activity)
   implementation(libs.androidx.constraintlayout)
+
+  // Firebase
+  implementation(platform(libs.firebase.bom))
+  implementation(libs.firebase.database)
+  implementation(libs.firebase.storage)
+  implementation(libs.firebase.auth)
+
   testImplementation(libs.junit)
   androidTestImplementation(libs.androidx.junit)
   androidTestImplementation(libs.androidx.espresso.core)
