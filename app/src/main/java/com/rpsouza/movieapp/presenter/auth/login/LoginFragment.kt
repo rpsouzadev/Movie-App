00fc65dc26d11140034e7a11ed88c6281e7a4fea
Rafael Souza <rpsouza.dev@gmail.com>
2024-05-21 18:01:54 -0300

    Add login screen layout

---
+++ app/src/main/java/com/rpsouza/movieapp/presenter/auth/login/LoginFragment.kt
@@ -0,0 +1,27 @@
+package com.rpsouza.movieapp.presenter.auth.login
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.rpsouza.movieapp.R
+import com.rpsouza.movieapp.databinding.FragmentLoginBinding
+
+class LoginFragment : Fragment() {
+  private var _binding: FragmentLoginBinding? = null
+  private val binding get() = _binding!!
+
+  override fun onCreateView(
+    inflater: LayoutInflater, container: ViewGroup?,
+    savedInstanceState: Bundle?
+  ): View {
+    _binding = FragmentLoginBinding.inflate(inflater, container, false)
+    return binding.root
+  }
+
+  override fun onDestroy() {
+    super.onDestroy()
+    _binding = null
+  }
+}
