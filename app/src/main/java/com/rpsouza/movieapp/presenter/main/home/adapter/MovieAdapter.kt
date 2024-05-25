ba31fb8d9c9047c49e47ffc8810799e11b4b1817
Rafael Souza <rpsouza.dev@gmail.com>
2024-05-25 15:13:32 -0300

    Add movie adapter

---
+++ app/src/main/java/com/rpsouza/movieapp/presenter/main/home/adapter/MovieAdapter.kt
@@ -0,0 +1,51 @@
+package com.rpsouza.movieapp.presenter.main.home.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.rpsouza.movieapp.databinding.MovieItemBinding
+import com.rpsouza.movieapp.domain.model.Movie
+
+class MovieAdapter(
+  private val context: Context
+) : ListAdapter<Movie, MovieAdapter.MyViewHolder>(DIFF_CALLBACK) {
+
+  companion object {
+    val DIFF_CALLBACK = object: DiffUtil.ItemCallback<Movie>() {
+      override fun areItemsTheSame(oldItem: Movie, newItem: Movie): Boolean {
+        return oldItem.id == newItem.id
+      }
+
+      override fun areContentsTheSame(oldItem: Movie, newItem: Movie): Boolean {
+        return oldItem == newItem
+      }
+
+    }
+  }
+
+  override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+    return MyViewHolder(
+      MovieItemBinding.inflate(
+        LayoutInflater.from(parent.context),
+        parent,
+        false
+      )
+    )
+  }
+
+  override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+    val movie = getItem(position)
+
+    Glide
+      .with(context)
+      .load("https://image.tmdb.org/t/p/w500${movie.posterPath}")
+      .into(holder.binding.movieImage)
+  }
+
+  inner class MyViewHolder(val binding: MovieItemBinding) : RecyclerView.ViewHolder(binding.root)
+
+}
