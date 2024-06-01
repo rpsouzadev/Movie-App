3cf41e7e68225b5f6f726fb72a6dad62ab223721
Rafael Souza <rpsouza.dev@gmail.com>
2024-06-01 10:42:51 -0300

    Add movie listing in MovieGenreFragment

--- app/src/main/java/com/rpsouza/movieapp/presenter/main/movieGenre/MovieGenreFragment.kt
+++ app/src/main/java/com/rpsouza/movieapp/presenter/main/movieGenre/MovieGenreFragment.kt
@@ -1,14 +1,17 @@
 package com.rpsouza.movieapp.presenter.main.movieGenre
 
 import android.os.Bundle
-import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.navArgs
-import com.rpsouza.movieapp.R
+import androidx.recyclerview.widget.GridLayoutManager
 import com.rpsouza.movieapp.databinding.FragmentMovieGenreBinding
+import com.rpsouza.movieapp.presenter.main.bottomBar.home.adapter.MovieAdapter
+import com.rpsouza.movieapp.utils.StateView
 import com.rpsouza.movieapp.utils.initToolbar
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -18,6 +21,8 @@ class MovieGenreFragment : Fragment() {
   private val binding get() = _binding!!
 
   private val args: MovieGenreFragmentArgs by navArgs()
+  private lateinit var movieAdapter: MovieAdapter
+  private val movieGenreViewModel: MovieGenreViewModel by viewModels()
 
   override fun onCreateView(
     inflater: LayoutInflater, container: ViewGroup?,
@@ -30,6 +35,36 @@ class MovieGenreFragment : Fragment() {
   override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
     super.onViewCreated(view, savedInstanceState)
     initToolbar(toolbar = binding.toolbar)
+    initRecycler()
+    getMovieByGenreList()
+  }
+
+  private fun initRecycler() {
+    movieAdapter = MovieAdapter(requireContext())
+
+    with(binding.recyclerMovies) {
+      layoutManager = GridLayoutManager(requireContext(), 2)
+      setHasFixedSize(true)
+      adapter = movieAdapter
+    }
+  }
+
+  private fun getMovieByGenreList() {
+    movieGenreViewModel.getMovieByGenreList(args.genreId).observe(viewLifecycleOwner) { stateView ->
+      when (stateView) {
+        is StateView.Loading -> {
+          binding.progressBar.isVisible = true
+        }
+        is StateView.Success -> {
+          binding.progressBar.isVisible = false
+          movieAdapter.submitList(stateView.data)
+        }
+
+        is StateView.Error -> {
+          binding.progressBar.isVisible = false
+        }
+      }
+    }
   }
 
   override fun onDestroy() {
---
+++ app/src/main/java/com/rpsouza/movieapp/presenter/main/movieGenre/MovieGenreViewModel.kt
@@ -0,0 +1,38 @@
+package com.rpsouza.movieapp.presenter.main.movieGenre
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.liveData
+import com.rpsouza.movieapp.BuildConfig
+import com.rpsouza.movieapp.domain.usecase.movie.GetMovieByGenreUseCase
+import com.rpsouza.movieapp.utils.Constants
+import com.rpsouza.movieapp.utils.StateView
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import retrofit2.HttpException
+import javax.inject.Inject
+
+@HiltViewModel
+class MovieGenreViewModel @Inject constructor(
+  private val getMovieByGenreUseCase: GetMovieByGenreUseCase
+): ViewModel() {
+
+  fun getMovieByGenreList(genreId: Int?) = liveData(Dispatchers.IO) {
+    try {
+      emit(StateView.Loading())
+
+      val moviesByGenre = getMovieByGenreUseCase.invoke(
+        apiKey = BuildConfig.THE_MOVIE_DB_KEY,
+        language = Constants.Movie.LANGUAGE,
+        genreId = genreId
+      )
+
+      emit(StateView.Success(data = moviesByGenre))
+    } catch (ex: HttpException) {
+      ex.printStackTrace()
+      emit(StateView.Error(message = ex.message))
+    } catch (ex: Exception) {
+      ex.printStackTrace()
+      emit(StateView.Error(message = ex.message))
+    }
+  }
+}
