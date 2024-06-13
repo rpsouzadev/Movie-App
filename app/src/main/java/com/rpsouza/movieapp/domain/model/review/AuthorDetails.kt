8d52946001ab7b768a77d39d5aa1e3edc3a4c44a
Rafael Souza <rpsouza.dev@gmail.com>
2024-06-12 22:57:05 -0300

    Add Mapping the film comment request

---
+++ app/src/main/java/com/rpsouza/movieapp/domain/model/review/AuthorDetails.kt
@@ -0,0 +1,8 @@
+package com.rpsouza.movieapp.domain.model.review
+
+data class AuthorDetails(
+    val avatarPath: String?,
+    val name: String?,
+    val rating: Int?,
+    val username: String?
+)
---
+++ app/src/main/java/com/rpsouza/movieapp/domain/model/review/MovieReview.kt
@@ -0,0 +1,14 @@
+package com.rpsouza.movieapp.domain.model.review
+
+import com.google.gson.annotations.SerializedName
+import com.rpsouza.movieapp.data.model.review.AuthorDetailsResponse
+
+data class MovieReview(
+    val author: String?,
+    val authorDetails: AuthorDetails?,
+    val content: String?,
+    val createdAt: String?,
+    val id: String?,
+    val updatedAt: String?,
+    val url: String?
+)
