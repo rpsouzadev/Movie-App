233b27d700e6baeb2b0821d1e0f2422bc65746b4
Rafael Souza <rpsouza.dev@gmail.com>
2024-06-20 20:42:38 -0300

    Add toolbar in download fragment

--- app/src/main/java/com/rpsouza/movieapp/presenter/main/bottomBar/download/DownloadFragment.kt
+++ app/src/main/java/com/rpsouza/movieapp/presenter/main/bottomBar/download/DownloadFragment.kt
@@ -1,28 +1,114 @@
 package com.rpsouza.movieapp.presenter.main.bottomBar.download
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.ferfalk.simplesearchview.SimpleSearchView
+import com.rpsouza.movieapp.MainGraphDirections
 import com.rpsouza.movieapp.R
 import com.rpsouza.movieapp.databinding.FragmentDownloadBinding
+import com.rpsouza.movieapp.presenter.main.bottomBar.download.adapter.DownloadMovieAdapter
+import com.rpsouza.movieapp.utils.hideKeyboard
+import com.rpsouza.movieapp.utils.initToolbar
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class DownloadFragment : Fragment() {
-  private var _binding: FragmentDownloadBinding? = null
-  private val binding get() = _binding!!
+    private var _binding: FragmentDownloadBinding? = null
+    private val binding get() = _binding!!
 
-  override fun onCreateView(
-    inflater: LayoutInflater,
-    container: ViewGroup?,
-    savedInstanceState: Bundle?
-  ): View {
-    _binding = FragmentDownloadBinding.inflate(inflater, container, false)
-    return binding.root
-  }
+    private lateinit var downloadMovieAdapter: DownloadMovieAdapter
 
-  override fun onDestroyView() {
-    super.onDestroyView()
-    _binding = null
-  }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentDownloadBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initToolbar(toolbar = binding.toolbar, showIconNavigation = false)
+        initRecycler()
+        initSearchView()
+    }
+
+    private fun initRecycler() {
+        downloadMovieAdapter = DownloadMovieAdapter(
+            context = requireContext(),
+            detailsClickListener = { movieId ->
+                val action = MainGraphDirections.actionGlobalMovieDetailsFragment(movieId)
+                findNavController().navigate(action)
+            },
+            deleteClickListener = { movieId ->
+
+            }
+        )
+
+        with(binding.recyclerDownload) {
+            setHasFixedSize(true)
+            adapter = downloadMovieAdapter
+        }
+    }
+
+    private fun initSearchView() {
+        binding.simpleSearchView.setOnQueryTextListener(object :
+            SimpleSearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String): Boolean {
+                hideKeyboard()
+                if (query.isNotEmpty()) {
+//          searchMovies(query)
+                }
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String): Boolean {
+                return false
+            }
+
+            override fun onQueryTextCleared(): Boolean {
+                return false
+            }
+        })
+
+        binding.simpleSearchView.setOnSearchViewListener(object :
+            SimpleSearchView.SearchViewListener {
+            override fun onSearchViewShown() {
+            }
+
+            override fun onSearchViewClosed() {
+//        getMovieByGenreList()
+            }
+
+            override fun onSearchViewShownAnimation() {
+            }
+
+            override fun onSearchViewClosedAnimation() {
+            }
+        })
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu_search_view, menu)
+        val item = menu.findItem(R.id.action_search)
+        binding.simpleSearchView.setMenuItem(item)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
